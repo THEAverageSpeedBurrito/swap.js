9f53fb68bc28c89ba966ade0dd20658b0f7c6479
Evan Busse <evanbusse@gmail.com>
2016-12-20 10:26:47 -0700

    reverse using swap

--- swap.js
+++ swap.js
@@ -8,13 +8,24 @@ function swap (array, index1, index2) {
   array[index1] = array[index2];
   array[index2] = v1;
 
-  console.log(`Swapping ${v1} & ${array[index1]}`);
   return array;
 }
 
 var testArray = [1, 5, 14, 'this', '16', false];
+var oddArray = [1, 2, 3, 4, 5, 6, 7];
 console.log('Original Array: ', testArray);
+console.log('Original Array: ', oddArray);
 
-console.log(swap(testArray, 1, 3));
-console.log(swap(testArray, 4, 1));
-console.log(swap(testArray, 2, 4));
+console.log('Swapping index 1 and 3: ', swap(testArray, 1, 3));
+
+function reverse (arr) {
+  let farIndex = arr.length - 1;
+  for(let i = 0; i < arr.length/2; i++){
+    swap(arr, i, length);
+    farIndex--;
+  }
+  return arr;
+}
+
+console.log(reverse(testArray));
+console.log(reverse(oddArray));
